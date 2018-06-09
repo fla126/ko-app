1055251c086d1fbfc880666aa1d25887b737df08
ljx <flyljxin@163.com>
2018-06-09 10:05:44 +0800

    样式修改

--- src/pages/mycenter/uinfo.vue
+++ src/pages/mycenter/uinfo.vue
@@ -6,19 +6,32 @@
       </router-link>
     </mt-header>
 
-    <div  class="common-content bg-white mt50">
+
+    <div class="common-content bg-white mt40 ft-c-gray">
       <div class="inner">
-        <mt-cell class="common-mt-cell ft-c-gray" title="用户协议" value="Vivining"></mt-cell>
-        <mt-cell class="common-mt-cell ft-c-gray" title="隐私条款" value="usbtkey123456"></mt-cell>
-        <mt-cell class="common-mt-cell ft-c-gray" title="加入我们" value="27"></mt-cell>
-        <mt-cell class="common-mt-cell ft-c-gray" title="常用地址" icon="field-warning" is-link></mt-cell>
+        <div class="box-cont " >
+          <div class="item"><span class="f30">用户协议</span></div>
+        </div>
+        <div class="box-cont " >
+          <div class="item"><span class="f30">隐私条款</span></div>
+        </div>
+        <div class="box-cont " >
+          <div class="item"><span class="f30">加入我们</span></div>
+        </div>
+        <div class="box-cont " >
+          <div class="item"><span class="f30">常用地址</span></div>
+        </div>
       </div>
     </div>
 
-    <div  class="common-content bg-white mt50">
+    <div  class="common-content bg-white mt50 ft-c-gray">
       <div class="inner">
-        <mt-cell class="common-mt-cell ft-c-gray" title="修改登录密码" is-link></mt-cell>
-        <mt-cell class="common-mt-cell ft-c-gray" title="退出登录"  is-link></mt-cell>
+        <div class="box-cont " >
+          <div class="item"><span class="f30">修改登录密码</span></div>
+        </div>
+        <div class="box-cont " >
+          <div class="item"><span class="f30">退出登录</span></div>
+        </div>
       </div>
     </div>
 
@@ -39,77 +52,24 @@
 </script>
 
 <style type="text/css" scoped >
-  #uinfo {
-
-  }
-  #uinfo .all-header .mintui-back{
-    font-size: 0.4rem;
-    color: #7f7f7f;
-  }
-
-
-  .all-header{
-    height: .95rem;
-    background-color: white;
-    border-bottom:.01rem solid #e4e5e7;
-  }
-  .all-header .right img{
+  .box-cont{
+    display: flex;
+    align-items: center;
+    height: .85rem;
+    border-bottom: .05rem solid #f9f9f9;
+  .item:first-child{
     width: .4rem;
+  }
+  .item{
     height: .4rem;
+  span{
+    line-height: .4rem;
+    margin-left: .3rem;
   }
-  .all-header h1{
-    font-size: .36rem;
-    color: #333333;
   }
-  .m-box1,.m-box2{
-    background-color:white;
   }
-  .m-box2{
-    margin-top: .4rem;
-    color: #4d7bf3;
-  }
-
-  .w-content>.inner{
-    padding: 0 .2rem;
-  }
-
-  .w-content a.w-box1{
-    margin-top: .4rem;
-  }
-
-  .w-content a.w-box1 .mint-cell-title img{
-    height:.3rem;
-    width: .4rem;
-  }
-  .w-content  a.w-box2 .mint-cell-title img{
-    height:.34rem;
-    width: .34rem;
-  }
-  .w-content a.w-box3 .mint-cell-title img{
-    height:.34rem;
-    width: .4rem;
-  }
-  .w-content a.w-box4 .mint-cell-title img{
-    height:.34rem;
-    width: .34rem;
-  }
-
-  /*帮助*/
-  .w-content a.w-box5 .mint-cell-title img{
-    height:.38rem;
-    width: .32rem;
-  }
-  .w-content a.w-box6 .mint-cell-title img{
-    height:.36rem;
-    width: .34rem;
-  }
-  .w-item .mint-cell-title .mint-cell-text{
-    font-size: .3rem;
-  }
-
-  .w-content .mint-cell{
-    min-height: .85rem;
-    border-bottom: .01rem solid #ecedee;
+  .box-cont:last-child{
+    border-bottom:0;
   }
 
 </style>
